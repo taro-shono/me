685577bf61ee9f87377d2c894c579545c85cfc31
hanagejet <taroage@gmail.com>
2019-03-09 01:03:27 +0900

    feat: add content

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.header`
+  margin: 40px 0 0;
+  @media (min-width: 768px) {
+    margin: 80px 0 0;
+  }
+`;
+const Title = styled.h1``;
+const Description = styled.p``;
+
+const Header: React.FC = () => (
+  <Container>
+    <Title>hanagejet</Title>
+    <Description>I&apos;m engineer</Description>
+  </Container>
+);
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,68 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import 'sanitize.css';
+import { withPrefix } from 'gatsby';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
-const Header = styled.header``;
+const Layout = styled.div`
+  padding: 0 16px;
+  @media (min-width: 768px) {
+    padding: 0 120px;
+  }
+`;
+const Content = styled.section`
+  margin: 72px 0 0;
+`;
+const ContentHeading = styled.h2`
+  margin: 0;
+`;
+const ContentList = styled.ul`
+  margin: 16px 0 0;
+  padding: 0;
+  list-style-position: inside;
+`;
+const ContentItem = styled.li`
+  margin: 8px 0 0;
+`;
+const Anchor = styled.a``;
+const Me = styled.img``;
 
-const Index = () => <Header>Hello world!</Header>;
+const Index: React.FC = () => (
+  <Layout>
+    <Header />
+    <Content>
+      <ContentHeading>About</ContentHeading>
+      <ContentList>
+        <ContentItem>Taro Shono</ContentItem>
+        <ContentItem>1987.07.31 / Tokyo</ContentItem>
+        <ContentItem>Joined web since 2012.11</ContentItem>
+        <ContentItem>
+          <Me src={withPrefix('/img/me.jpg')} width="112" alt="Taro Shono" />
+        </ContentItem>
+      </ContentList>
+    </Content>
+    <Content>
+      <ContentHeading>Contact</ContentHeading>
+      <ContentList>
+        <ContentItem>
+          <Anchor href="http://github.com/hanagejet/" target="_blank">
+            http://github.com/hanagejet/
+          </Anchor>
+        </ContentItem>
+        <ContentItem>
+          <Anchor href="https://twitter.com/hanagejet" target="_blank">
+            https://twitter.com/hanagejet
+          </Anchor>
+        </ContentItem>
+        <ContentItem>
+          <Anchor href="https://www.instagram.com/hanagejet/" target="_blank">
+            https://www.instagram.com/hanagejet/
+          </Anchor>
+        </ContentItem>
+      </ContentList>
+    </Content>
+    <Footer />
+  </Layout>
+);
 export default Index;
